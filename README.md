604fb7659d3d941fa86374e5c44c095d390dbe53
B1tF8er <ofeth@hotmail.com>
2019-05-14 22:22:42 -0600

    Expand delegates samples

--- README.md
+++ README.md
@@ -29,8 +29,15 @@ using System.Diagnostics;
 public static class TestDelegates
 {
     public delegate void LoggerDelegate(string message);
+    public delegate string LoggerWithResponseDelegate(string message);
 
     public static void Run()
+    {
+        RunVoidDelegates();
+        RunResponseDelegates();
+    }
+
+    public static void RunVoidDelegates()
     {
         LoggerDelegate consoleLoggerHandler = ConsoleLogger;
         LoggerDelegate debugLoggerHandler = DebugLogger;
@@ -41,9 +48,32 @@ public static class TestDelegates
         allConsoleHandlers("this goes to all");
     }
 
+    public static void RunResponseDelegates()
+    {
+        LoggerWithResponseDelegate consoleLoggerHandler = ConsoleLoggerWithResponse;
+        LoggerWithResponseDelegate debugLoggerHandler = DebugLoggerWithResponse;
+        LoggerWithResponseDelegate allConsoleHandlers = consoleLoggerHandler + debugLoggerHandler;
+
+        var consoleResponse = consoleLoggerHandler("This goes to the console");
+        var debugResponse = debugLoggerHandler("This goes to the debug");
+        var lastResponse = allConsoleHandlers("this goes to all");
+    }
+
     public static void ConsoleLogger(string message) => Console.WriteLine(message);
 
     public static void DebugLogger(string message) => Debug.WriteLine(message);
+
+    public static string ConsoleLoggerWithResponse(string message)
+    {
+        Console.WriteLine(message);
+        return "Logged to console";
+    }
+
+    public static string DebugLoggerWithResponse(string message)
+    {
+        Debug.WriteLine(message);
+        return "Logged to debug";
+    }
 }
 ```
 
--- src/fn/TestDelegates.cs
+++ src/fn/TestDelegates.cs
@@ -6,8 +6,15 @@ namespace fn
     public static class TestDelegates
     {
         public delegate void LoggerDelegate(string message);
+        public delegate string LoggerWithResponseDelegate(string message);
 
         public static void Run()
+        {
+            RunVoidDelegates();
+            RunResponseDelegates();
+        }
+
+        public static void RunVoidDelegates()
         {
             LoggerDelegate consoleLoggerHandler = ConsoleLogger;
             LoggerDelegate debugLoggerHandler = DebugLogger;
@@ -18,8 +25,31 @@ namespace fn
             allConsoleHandlers("this goes to all");
         }
 
+        public static void RunResponseDelegates()
+        {
+            LoggerWithResponseDelegate consoleLoggerHandler = ConsoleLoggerWithResponse;
+            LoggerWithResponseDelegate debugLoggerHandler = DebugLoggerWithResponse;
+            LoggerWithResponseDelegate allConsoleHandlers = consoleLoggerHandler + debugLoggerHandler;
+
+            var consoleResponse = consoleLoggerHandler("This goes to the console");
+            var debugResponse = debugLoggerHandler("This goes to the debug");
+            var lastResponse = allConsoleHandlers("this goes to all");
+        }
+
         public static void ConsoleLogger(string message) => Console.WriteLine(message);
 
         public static void DebugLogger(string message) => Debug.WriteLine(message);
+
+        public static string ConsoleLoggerWithResponse(string message)
+        {
+            Console.WriteLine(message);
+            return "Logged to console";
+        }
+
+        public static string DebugLoggerWithResponse(string message)
+        {
+            Debug.WriteLine(message);
+            return "Logged to debug";
+        }
     }
 }
