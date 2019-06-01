8a98af9d884a1f7598d859459b664e78e75faaa4
B1tF8er <ofeth@hotmail.com>
2019-06-01 16:28:16 -0600

    Change Alias

--- src/fn/MathExtensions.Division.cs
+++ src/fn/MathExtensions.Division.cs
@@ -2,16 +2,16 @@ namespace fn
 {
     using static Constants.Numbers;
 
-    using UnaryF = System.Func<int, int>;
-    using BinaryF = System.Func<int, System.Func<int, int>>;
+    using Divide = System.Func<int, System.Func<int, int>>;
+    using DividedBy = System.Func<int, int>;
 
     internal static partial class MathExtensions
     {
         internal static class Division
         {
-            private static BinaryF Divide() => (divisor) => (dividend) => dividend / divisor;
+            private static Divide Divide() => (divisor) => (dividend) => dividend / divisor;
             internal static int Divide((int, int) factors) => Divide()(factors.Item1)(factors.Item2);
-            internal static UnaryF DivideyBy(int divisor) => Divide()(divisor);
+            internal static DividedBy DivideyBy(int divisor) => Divide()(divisor);
             internal static int ByOne(int dividend) => DivideyBy(One)(dividend);
             internal static int ByTwo(int dividend) => DivideyBy(Two)(dividend);
             internal static int ByThree(int dividend) => DivideyBy(Three)(dividend);
--- src/fn/MathExtensions.Multiplication.cs
+++ src/fn/MathExtensions.Multiplication.cs
@@ -2,16 +2,16 @@ namespace fn
 {
     using static Constants.Numbers;
 
-    using UnaryF = System.Func<int, int>;
-    using BinaryF = System.Func<int, System.Func<int, int>>;
+    using Multiply = System.Func<int, System.Func<int, int>>;
+    using MultiplyBy = System.Func<int, int>;
 
     internal static partial class MathExtensions
     {
         internal static class Multiplication
         {
-            private static BinaryF Multiply() => (multiplicand) => (multiplier) => multiplicand * multiplier;
+            private static Multiply Multiply() => (multiplicand) => (multiplier) => multiplicand * multiplier;
             internal static int Multiply((int, int) factors) => Multiply()(factors.Item1)(factors.Item2);
-            internal static UnaryF MultiplyBy(int multiplicand) => Multiply()(multiplicand);
+            internal static MultiplyBy MultiplyBy(int multiplicand) => Multiply()(multiplicand);
             internal static int Single(int multiplier) => MultiplyBy(One)(multiplier);
             internal static int Double(int multiplier) => MultiplyBy(Two)(multiplier);
             internal static int Triple(int multiplier) => MultiplyBy(Three)(multiplier);
